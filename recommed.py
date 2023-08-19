987714433b53c4c96fc20785107b5546f21a8e86
soham2312 <94427845+soham2312@users.noreply.github.com>
2023-08-19 17:29:29 +0530

    Update recommed.py
    changes done

--- recommed.py
+++ recommed.py
@@ -14,6 +14,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
    
 @app.route('/predict', methods=['POST'])
 def predict():
+   print("fuck")
     data = request.get_json(force=True)
     data=data['products']
     # print(data)
